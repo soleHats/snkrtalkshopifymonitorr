cae967d93ac6920aac0c5fb6458c080895c21798
Greyson Hurst <34924247+greysonn@users.noreply.github.com>
2018-02-15 16:14:41 -0600

    Add files via upload

---
+++ src/monitor.js
@@ -0,0 +1,184 @@
+const config = require('../config');
+const request = require('request-promise');
+const _ = require('underscore');
+const chalk = require('chalk');
+const cheerio = require('cheerio');
+const events = require('./events');
+
+class init {
+  constructor(site) {
+      var lastone = '';
+      var cycle = 0;
+      var original = [];
+      var newprod = [];
+      var proxylist = [];
+      var fs = require('fs');
+      var array = fs.readFileSync(__dirname + '/../proxies.txt').toString().split("\n");
+      for (var i in array) {
+        if (array[i] === '') {
+
+        } else {
+          proxylist.push(array[i])
+        }
+      }
+
+      function getproxy() {
+        if (proxylist.length == 0) {
+          var proxy = 'http://localhost'
+          return proxy;
+        } else {
+          var ogprox = proxylist[Math.floor(Math.random() * proxylist.length)]
+          if (ogprox.split(':')[2] == undefined) {
+            var proxy = `http://${ogprox.split(':')[0]}:${ogprox.split(':')[1]}`
+            return proxy;
+          } else {
+            var proxy = `http://${ogprox.split(':')[2]}:${ogprox.split(':')[3]}@${ogprox.split(':')[0]}:${ogprox.split(':')[1]}`
+            return proxy;
+          }
+        }
+        //console.log(proxy);
+      }
+
+      function diff(one, two) {
+        return _.reject(one, _.partial(_.findWhere, two, _));
+      }
+      //console.log(site);
+      //console.log(diff(one, two));
+      initialize()
+      function initialize() {
+        //console.log('yo ' + site);
+        var prox = getproxy()
+        //console.log(prox);
+        var mainurl;
+        if (site.startsWith('http') == true) {
+          if (site.endsWith('/') == true) {
+            mainurl = `${site}sitemap_products_1.xml`
+          } else {
+            mainurl = `${site}/sitemap_products_1.xml`
+          }
+        } else {
+          if (site.endsWith('/') == true) {
+            mainurl = `https://${site}sitemap_products_1.xml`
+          } else {
+            mainurl = `https://${site}/sitemap_products_1.xml`
+          }
+        }
+        const opts = {
+          method: 'GET',
+          uri: mainurl,
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+          },
+          gzip: true,
+          proxy: prox,
+          json: true,
+          simple: false
+        }
+        request(opts)
+          .then(function(data) {
+          //console.log(data);
+          //console.log(`Cycle - ${cycle}`);
+            if (cycle == 0) {
+              console.log(chalk.green('Initialized - ' + site));
+            }
+            if (data.toString().indexOf("Try again in a couple minutes") != -1) {
+              console.log(chalk.red('You are banned try again later. - ' + site));
+              //console.log(data);
+            } else {
+              //console.log(cycle);
+              var $ = cheerio.load(data);
+              if (cycle == 0) {
+                var arr = original;
+                //console.log('og');
+              } else {
+                var arr = newprod;
+                //console.log('newprod');
+              }
+                $('urlset url').each(function() {
+                  var lastmod = $(this).children('lastmod').text()
+                  var url = $(this).children('loc').text()
+                  var children = $(this).children();
+                  var imgchild = $(children).children();
+                  var title = $(imgchild[1]).text()
+                  if (title === '') {
+                    data = {
+                      url: url,
+                      title: "Title Missing",
+                      time: lastmod
+                    }
+                    arr.push(data)
+                  } else {
+                    data = {
+                      url: url,
+                      title: title,
+                      time: lastmod
+                    }
+                    arr.push(data)
+                    //console.log(`${title} - ${lastmod}`);
+                  }
+
+                });
+                if (cycle == 0) {
+
+                } else {
+                  //console.log(newprod.length);
+                  //console.log(original.length);
+                  if (newprod.length > original.length) {
+                    console.log('New Item - ' + site);
+                    //console.log(diff(newprod, original));
+                    events.emit('newitem', {
+                      url: diff(newprod, original)[0].url
+                    });
+                    original = newprod
+                  }
+
+                  if (newprod.length < original.length) {
+                    original = newprod
+                  }
+
+                  if (newprod.length == original.length) {
+                    //console.log('Same burv');
+                    for (var i = 0; i < newprod.length; i++) {
+                      if (newprod[i].time.toString() === original[i].time.toString()) {
+                        //console.log(`${newprod[i].time} - ${original[i].time}`);
+                      } else {
+                        if (lastone === '') {
+                          lastone = newprod[i].url
+                          events.emit('restock', {
+                            url: newprod[i].url,
+                            time: newprod[i].time.toString()
+                          });
+                        } else {
+                          if (lastone === newprod[i].url) {
+                          } else {
+                            lastone = newprod[i].url
+                            events.emit('restock', {
+                              url: newprod[i].url,
+                              time: newprod[i].time.toString()
+                            });
+                          }
+                        }
+                      }
+                    }
+                    original = newprod;
+                  }
+                }
+                cycle++;
+                setTimeout(function() {
+                  newprod = [];
+                  initialize()
+                }, config.delay)
+            }
+          })
+          .catch(function(err) {
+            //console.log('Err - ' + site);
+          })
+      }
+  }
+}
+
+
+
+module.exports = {
+    init: init
+};
---
+++ src/webhook.js
@@ -0,0 +1,155 @@
+const request = require('request-promise');
+const config = require('../config');
+exports.send = function send (webhook, product, type, timestamp) {
+  sent = false;
+  const opts = {
+    method: 'GET',
+    uri: product,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+    },
+    gzip: true,
+    json: true,
+    simple: false
+  }
+  request(opts)
+    .then(function(response) {
+      var real = false;
+      if (type === 'restock') {
+        for (var i = 0; i < response.product.variants.length; i++) {
+          if (response.product.variants[i].updated_at === timestamp) {
+            if (real == false) {
+              real = true;
+            }
+          }
+        }
+        if (real == true) {
+          start()
+        } else {
+
+        }
+      } else {
+        start()
+      }
+      function start() {
+        links = ''
+        for (var i = 0; i < response.product.variants.length; i++) {
+          if (response.product.variants[i].inventory_quantity == undefined) {
+            stock = 'Unavailable'
+          } else if (response.product.variants[i].inventory_quantity == 0) {
+            stock = 'Sold Out'
+          } else {
+            stock = response.product.variants[i].inventory_quantity
+          }
+          if (response.product.variants[i].updated_at === timestamp) {
+            links += `[${response.product.variants[i].title} / Stock: ${stock}](http://${product.split('://')[1].split('/')[0]}/cart/${response.product.variants[i].id}:1)\n`
+          }
+        }
+        if (type === 'restock') {
+          type = 'Restock'
+        } else {
+          type = 'New Item'
+        }
+        if (response.product.images[0] == undefined) {
+          imageurl = 'http://mydaymyplan.com/images/no-image-large.png'
+        } else {
+          imageurl = response.product.images[0].src.replace("\/", "/");
+        }
+        var prebrand = response.product.vendor;
+        var brand = prebrand.toLowerCase()
+
+        if (config.keywords.length > 0) {
+          var matched = false;
+          for (var i = 0; i < config.keywords.length; i++) {
+            if (response.product.title.toLowerCase().indexOf(config.keywords[i].toLowerCase()) != -1) {
+              if (matched == false) {
+                matched = true;
+              }
+            }
+          }
+          if (matched == true) {
+            hookit()
+            if (sent == true) {
+
+            } else {
+              sent = true;
+              console.log('Restock: ' + response.product.title.replace("\/", "/") + ' - ' + product.split("/produ")[0].split('//')[1]);
+            }
+          } else {
+            //console.log('Na dawg it didnt - ' + response.product.title);
+          }
+        } else {
+          hookit()
+        }
+        function hookit() {
+          //console.log('Im finna hook');
+          if (type === 'Restock') {
+            color = 16749381
+          } else {
+            color = 65440
+          }
+          const opts = {
+            method: 'POST',
+            uri: webhook,
+            json: true,
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: {
+              "embeds": [{
+                "color": color,
+                "title" : response.product.title.replace("\/", "/"),
+                "url": product,
+                "thumbnail": {
+                  "url": imageurl
+                },
+                "footer": {
+                  "icon_url": "https://cdn.shopify.com/s/files/1/1061/1924/products/Heart_Eyes_Emoji_2_large.png?v=1513251039",
+                  "text": "BotSPLY Shopify Monitor made by atombot"
+                },
+                "fields": [
+                  {
+                    "name": "Site",
+                    "value": product.split('://')[1].split('/')[0],
+                    "inline": true
+                  },
+                  {
+                    "name": "Type",
+                    "value": type,
+                    "inline": true
+                  },
+                  {
+                    "name": "Price",
+                    "value": response.product.variants[0].price,
+                    "inline": true
+                  },
+          				{
+          				  "name": "Brand",
+                    "value": response.product.vendor,
+                    "inline": true
+          				},
+                  {
+                    "name": "Checkout Links",
+                    "value": links,
+                    "inline": true
+                  }
+                ]
+              }]
+            }
+          }
+          request(opts)
+          .then(function(response) {
+            //console.log('Ya i hooked');
+            sent = false;
+          })
+          .catch(function(e) {
+            console.log('getting err');
+            console.log(e.message);
+            setTimeout(function() {
+              send(webhook, product, type);
+            }, 10000)
+          })
+      }
+      }
+    })
+}
